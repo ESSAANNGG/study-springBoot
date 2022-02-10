6fe5e6826e5685323e7957cb694a7630efa02926
LSJ97 <tkdwnsdk11@gmail.com>
2022-02-11 00:17:43 +0900

    querydsl 설정 Q파일 추가완료

--- guestbook/src/main/java/com/zerock/guestbook/repository/GuestBookRepository.java
+++ guestbook/src/main/java/com/zerock/guestbook/repository/GuestBookRepository.java
@@ -2,6 +2,7 @@ package com.zerock.guestbook.repository;
 
 import com.zerock.guestbook.entity.GuestBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestBookRepository extends JpaRepository<GuestBook, Long> {
+public interface GuestBookRepository extends JpaRepository<GuestBook, Long>, QuerydslPredicateExecutor<GuestBook> {
 }

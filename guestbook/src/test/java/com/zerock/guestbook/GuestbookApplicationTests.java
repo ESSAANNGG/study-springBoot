b355262d51cf9545f15d21f10e9d5d0e5f639549
LSJ97 <tkdwnsdk11@gmail.com>
2022-02-13 00:19:07 +0900

    등록,수정 테스트코드 생성

--- guestbook/src/test/java/com/zerock/guestbook/GuestbookApplicationTests.java
+++ guestbook/src/test/java/com/zerock/guestbook/GuestbookApplicationTests.java
@@ -1,7 +1,16 @@
 package com.zerock.guestbook;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.zerock.guestbook.entity.GuestBook;
+import com.zerock.guestbook.entity.QGuestBook;
+import com.zerock.guestbook.repository.GuestBookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @SpringBootTest
 class GuestbookApplicationTests {
@@ -10,4 +19,27 @@ class GuestbookApplicationTests {
     void contextLoads() {
     }
 
+    /*@Test
+    public void testQuery2(){
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("gno").descending());
+
+        QGuestBook qGuestBook = QGuestBook.guestBook;
+
+        String keyword = "1";
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        BooleanExpression exTitle = qGuestBook.title.contains(keyword);
+
+        BooleanExpression exContent = qGuestBook.content.contains(keyword);
+
+        BooleanExpression exALl = exTitle.or(exContent); // 1-----------------
+
+        booleanBuilder.and(exALl);
+
+        booleanBuilder.and(qGuestBook.gno.gt(0L));
+
+        Page<GuestBook> result = GuestBookRepository.findAll(booleanBuilder, pageable);
+    }*/
 }
---
+++ guestbook/src/test/java/com/zerock/guestbook/repository/GuestBookRepositoryTests.java
@@ -0,0 +1,45 @@
+package com.zerock.guestbook.repository;
+
+import com.zerock.guestbook.entity.GuestBook;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class GuestBookRepositoryTests {
+
+    @Autowired
+    private GuestBookRepository guestBookRepository;
+
+    @Test
+    public void insertDummies(){
+
+        IntStream.rangeClosed(1,300).forEach(i -> {
+            GuestBook guestBook = GuestBook.builder()
+                    .title("Title......." + i)
+                    .content("Content......." + i)
+                    .writer("user" + (i%10))
+                    .build();
+            System.out.println(guestBookRepository.save(guestBook));
+        });
+    }
+
+    @Test
+    public void updateTest(){
+
+        Optional<GuestBook> result = guestBookRepository.findById(300L);
+
+        if(result.isPresent()) {
+
+            GuestBook guestBook = result.get();
+
+            guestBook.changeTitle("Changed Title.......");
+            guestBook.changeContent("Changed Content.......");
+
+            guestBookRepository.save(guestBook);
+        }
+    }
+}

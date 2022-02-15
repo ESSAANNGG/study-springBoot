70a9333b33d9557dfb5b7b076638382537047419
LSJ97 <tkdwnsdk11@gmail.com>
2022-02-15 23:18:02 +0900

    page 결과 처리

--- guestbook/src/main/java/com/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/com/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -5,7 +5,6 @@ import com.zerock.guestbook.entity.GuestBook;
 import com.zerock.guestbook.repository.GuestBookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service

b6d448ffb37256e750cc63e634141ab92d2e6474
LSJ97 <tkdwnsdk11@gmail.com>
2022-02-16 22:32:44 +0900

    목록처리

--- guestbook/src/main/java/com/zerock/guestbook/service/GuestbookService.java
+++ guestbook/src/main/java/com/zerock/guestbook/service/GuestbookService.java
@@ -1,6 +1,8 @@
 package com.zerock.guestbook.service;
 
 import com.zerock.guestbook.dto.GuestbookDTO;
+import com.zerock.guestbook.dto.PageRequestDTO;
+import com.zerock.guestbook.dto.PageResultDTO;
 import com.zerock.guestbook.entity.GuestBook;
 
 public interface GuestbookService {
@@ -15,4 +17,19 @@ public interface GuestbookService {
                 .build();
         return entity;
     }
+
+    default GuestbookDTO entityToDto(GuestBook entity){
+
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+        return dto;
+    }
+
+    PageResultDTO<GuestbookDTO, GuestBook> getList(PageRequestDTO requestDTO);
 }
--- guestbook/src/main/java/com/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/com/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,11 +1,18 @@
 package com.zerock.guestbook.service;
 
 import com.zerock.guestbook.dto.GuestbookDTO;
+import com.zerock.guestbook.dto.PageRequestDTO;
+import com.zerock.guestbook.dto.PageResultDTO;
 import com.zerock.guestbook.entity.GuestBook;
 import com.zerock.guestbook.repository.GuestBookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 @Log4j2
@@ -27,5 +34,16 @@ public class GuestbookServiceImpl implements GuestbookService{
         repository.save(entity);
 
         return entity.getGno();
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, GuestBook> getList(PageRequestDTO requestDTO){
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+
+        Page<GuestBook> result = repository.findAll(pageable);
+
+        Function<GuestBook, GuestbookDTO> fn = (entity -> entityToDto(entity));
+
+        return new PageResultDTO<>(result, fn);
     }
 }
